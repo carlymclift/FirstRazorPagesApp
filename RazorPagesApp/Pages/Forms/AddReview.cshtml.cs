b780b6a4945ceba960ab97c38cc2ca3f4d5a7e46
Carly Clift <carlymclift@gmail.com>
2021-02-06 00:19:40 -0700

    Add review property and onPost method

--- RazorPagesApp/Pages/Forms/AddReview.cshtml.cs
+++ RazorPagesApp/Pages/Forms/AddReview.cshtml.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesApp.Models;
 
 namespace RazorPagesApp.Pages.Forms
 {
     public class AddReviewModel : PageModel
     {
+        [BindProperty]
+        public ReviewModel Review { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+            return RedirectToPage("/Index", new
+                {
+                    ReviewTitle = Review.ReviewTitle,
+                    ReviewerName = Review.ReviewerName,
+                    MovieTitle = Review.MovieTitle,
+                    Stars = Review.Stars,
+                    ReviewBody = Review.ReviewBody
+                });
+
+            }
+
+            return Page();
         }
     }
 }
