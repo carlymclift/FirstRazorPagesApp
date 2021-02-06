74893cf5b7313e0e99630fbc8e624f37458d0c45
Carly Clift <carlymclift@gmail.com>
2021-02-06 00:17:25 -0700

    Create review object with requirements

--- RazorPagesApp/Models/ReviewModel.cs
+++ RazorPagesApp/Models/ReviewModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace RazorPagesApp.Models
 {
     public class ReviewModel
     {
-        public ReviewModel()
-        {
-        }
+        [Required(ErrorMessage ="Please include a name so we know who reviewed this movie"),
+            MinLength(2)]
+        public string ReviewerName { get; set; }
+        [Required(ErrorMessage ="You must specify the movie you are reviewing"),
+            MinLength(2)]
+        public string MovieTitle { get; set; }
+        [Required(ErrorMessage ="Give your review a title"),
+            MinLength(2)]
+        public string ReviewTitle { get; set; }
+        public string Stars { get; set; }
+        [Required(ErrorMessage ="Please include your review"),
+            MinLength(20, ErrorMessage ="Your review must be at least 20 charaters long. Don't be shy! Tell us what you think."),
+            MaxLength(300, ErrorMessage ="Your review must be less than 300 charaters.")]
+        public string ReviewBody { get; set; }
     }
 }
